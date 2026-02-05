fdad34a727254d4802efb1c79e760076040a3556
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:29 +0500

    Add Label.vue

---
+++ src/components/Label.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  for?: string
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  class: '',
+})
+
+const classes = computed(() => {
+  return [
+    'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+    props.class,
+  ].join(' ')
+})
+</script>
+
+<template>
+  <label :for="props.for" :class="classes">
+    <slot />
+  </label>
+</template>
