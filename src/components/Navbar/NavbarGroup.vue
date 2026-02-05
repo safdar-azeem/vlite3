2328cfb5523cc582154fbc3c932ef415d5018c04
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:31 +0500

    Add NavbarGroup.vue

---
+++ src/components/Navbar/NavbarGroup.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  orientation?: 'horizontal' | 'vertical'
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  orientation: 'horizontal',
+  class: '',
+})
+
+const classes = computed(() => {
+  return [
+    'flex',
+    props.orientation === 'vertical' ? 'flex-col space-y-1' : 'flex-row items-center space-x-1',
+    props.class,
+  ].join(' ')
+})
+</script>
+
+<template>
+  <div :class="classes">
+    <slot />
+  </div>
+</template>
