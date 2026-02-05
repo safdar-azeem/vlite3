9116226f5ae59a91979b9d0cd537ed781a099ed2
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:33 +0500

    Add Tooltip.vue

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ToolTipLite from 'v-tooltip-lite'
+import 'v-tooltip-lite/style.css'
+import type { TooltTipProps } from 'v-tooltip-lite/types'
+
+const props = withDefaults(defineProps<TooltTipProps>(), {
+  placement: 'top',
+  class: '',
+  teleport: true,
+  arrow: true,
+  contentClass: 'max-w-[350px]',
+  triggerClass: '',
+})
+</script>
+
+<template>
+  <ToolTipLite v-bind="props" class="inline-block" class-name="basic-tooltip">
+    <template #trigger>
+      <slot />
+    </template>
+    <template #default>
+      <slot name="content" />
+    </template>
+  </ToolTipLite>
+</template>
