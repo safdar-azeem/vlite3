061ef0dfa52297e6074fb68ce21ada5fd314b146
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:39 +0500

    Add styles.ts

---
+++ src/types/styles.ts
@@ -0,0 +1,42 @@
+export type IVariants =
+  | 'danger'
+  | 'primary'
+  | 'default'
+  | 'outline'
+  | 'secondary'
+  | 'transparent'
+  | 'success'
+  | 'warning'
+
+export type ISize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full'
+
+export type IDropdownOption = {
+  label: string
+  value?: any
+  subtitle?: string
+  description?: string
+  icon?: string
+  emoji?: string
+  disabled?: Boolean
+  iconClass?: string
+  iconStyle?: any
+  length?: number
+  values?: any
+  data?: any
+  // Schema support for nested menus
+  children?: IDropdownOption[]
+  key?: string // Property name for nested value objects
+  position?: any // TooltTipPlacement
+  offset?: [number, number]
+  confirmation?:
+    | boolean
+    | {
+        title?: string
+        description?: string
+        confirmText?: string
+        cancelText?: string
+        variant?: 'danger' | 'primary' | 'warning' | 'success' | 'info'
+      }
+}
+
+export type IDropdownOptions = IDropdownOption[]
