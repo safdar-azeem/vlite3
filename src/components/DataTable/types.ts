bfadb74ac25b4b317a51f5401e7692f2ad66c102
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:26 +0500

    Add types.ts

---
+++ src/components/DataTable/types.ts
@@ -0,0 +1,85 @@
+export interface PaginationConfig {
+  page: number
+  limit: number
+}
+
+export interface SortConfig {
+  field: string
+  order: 'asc' | 'desc' | ''
+}
+
+export interface TableState {
+  pagination: PaginationConfig
+  sorting: SortConfig
+  search: string
+}
+
+export interface FilterConfig {
+  search: string
+  sorting: SortConfig
+  [key: string]: any
+}
+
+export interface TableFilter {
+  pagination: PaginationConfig
+  filter: FilterConfig
+}
+
+export interface TableHeader {
+  field: string
+  title: string
+  width?: string
+  minWidth?: string
+  sortable?: boolean
+  hideOnMobile?: boolean
+  align?: 'left' | 'center' | 'right'
+  format?: (value: any, row?: any) => string
+  class?: string | ((value: any, row?: any) => string)
+  capitalize?: boolean
+  addStatusColor?: boolean
+  type?: 'text' | 'price' | 'date' | 'number'
+}
+
+export interface PageInfo {
+  currentPage: number
+  totalPages: number
+  totalItems: number
+  itemsPerPage?: number
+}
+
+export interface DataTableProps {
+  rows: any[]
+  selectedRows?: any[]
+  // Toolbar Props
+  search?: string
+  showSearch?: boolean
+  searchPlaceholder?: string
+  headers: TableHeader[]
+  keyField?: string
+  loading?: boolean
+  selectable?: boolean
+  emptyMessage?: string
+  emptyIcon?: string
+  showPagination?: boolean
+  pageInfo?: PageInfo
+  itemsPerPage?: number
+  itemsPerPageOptions?: number[]
+  showItemsPerPage?: boolean
+  striped?: boolean
+  hoverable?: boolean
+  bordered?: boolean
+  compact?: boolean
+  tableClass?: string
+  class?: string
+}
+
+export interface SelectionState {
+  selected: any[]
+  all: boolean
+  indeterminate: boolean
+}
+
+export interface RowClickPayload {
+  row: any
+  index: number
+}
