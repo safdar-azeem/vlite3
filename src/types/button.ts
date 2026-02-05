6e92b79ace6208a0f463e919c7ccec4df8f81001
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:39 +0500

    Add button.ts

---
+++ src/types/button.ts
@@ -0,0 +1,36 @@
+export type ButtonVariant =
+  | 'primary'
+  | 'primary-light'
+  | 'secondary'
+  | 'danger'
+  | 'danger-light'
+  | 'warning'
+  | 'warning-light'
+  | 'info'
+  | 'info-light'
+  | 'success'
+  | 'success-light'
+  | 'outline'
+  | 'outline-primary'
+  | 'outline-danger'
+  | 'outline-warning'
+  | 'outline-info'
+  | 'outline-success'
+  | 'ghost'
+  | 'link'
+
+export type ButtonSize = 'xs' | 'md' | 'sm' | 'lg' | 'xl'
+export type ButtonRounded = 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full'
+
+export interface ButtonProps {
+  variant?: ButtonVariant
+  size?: ButtonSize
+  class?: any
+  disabled?: boolean
+  loading?: boolean
+  icon?: string
+  iconRight?: string
+  type?: 'button' | 'submit' | 'reset'
+  text?: string
+  rounded?: ButtonRounded
+}
