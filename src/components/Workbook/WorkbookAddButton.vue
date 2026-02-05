9d84cbd54ff718ce548673e7472d8d4c764fd812
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:33 +0500

    Add WorkbookAddButton.vue

---
+++ src/components/Workbook/WorkbookAddButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from '@/components/Button.vue'
+
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
+</script>
+
+<template>
+  <Button
+    variant="ghost"
+    size="sm"
+    icon="lucide:plus"
+    title="Add New Sheet"
+    rounded="none"
+    @click="emit('click', $event)" />
+</template>
