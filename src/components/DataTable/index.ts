639e0111067b8c9beb6627bd28a050311ac5ec98
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:26 +0500

    Add index.ts

---
+++ src/components/DataTable/index.ts
@@ -0,0 +1,5 @@
+export { default as DataTable } from './DataTable.vue'
+export { default as DataTableHeader } from './DataTableHeader.vue'
+export { default as DataTableRow } from './DataTableRow.vue'
+
+export * from './types'
