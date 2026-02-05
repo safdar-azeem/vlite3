0a6df1019ead8a155a436a47e2a48cec9d28512b
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 18:08:26 +0500

    Add DropdownTrigger.vue

---
+++ src/components/Dropdown/DropdownTrigger.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Button from '@/components/Button.vue'
+
+interface Props {
+  selectedLabel?: string
+  placeholder?: string
+  isOpen?: boolean
+  disabled?: boolean
+  className?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  selectedLabel: '',
+  placeholder: 'Select an option',
+  disabled: false,
+})
+</script>
+
+<template>
+  <Button
+    :text="selectedLabel || placeholder"
+    iconRight="iconamoon:arrow-down-2"
+    variant="outline"
+    :disabled="disabled"
+    class="w-full justify-between! font-normal"
+    :class="className" />
+</template>
