f357066e1f71364a4b7854e1165813e377c1a0d7
safdar-azeem <safdarazeem00@gmail.com>
2026-02-09 17:00:57 +0500

    Update CheckBox.vue

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -40,9 +40,9 @@ const toggle = () => {
 const sizeClasses: Record<CheckboxSize, string> = {
   xs: 'h-3.5 w-3.5',
   sm: 'h-4 w-4',
-  md: 'h-5 w-5',
-  lg: 'h-6 w-6',
-  xl: 'h-7 w-7',
+  md: 'h-4.5 w-4.5',
+  lg: 'h-5 w-5',
+  xl: 'h-5.5 w-5.5',
 }
 
 const iconSizeClasses: Record<CheckboxSize, string> = {
