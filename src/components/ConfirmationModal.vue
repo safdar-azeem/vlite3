2fd01bc91f6a24063a70aa4da289e61567ba992d
safdar-azeem <safdarazeem00@gmail.com>
2026-02-11 04:48:24 +0500

    Update ConfirmationModal.vue

--- src/components/ConfirmationModal.vue
+++ src/components/ConfirmationModal.vue
@@ -3,7 +3,7 @@ import Modal from './Modal.vue'
 import Button from './Button.vue'
 
 interface Props {
-  show: boolean
+  show?: boolean
   title: string
   description: string
   loading?: boolean
@@ -38,6 +38,11 @@ const close = () => {
     max-width="max-w-[400px]"
     @close="close"
     @update:show="(val) => !val && close()">
+    <template #trigger>
+      <slot name="trigger">
+        <slot />
+      </slot>
+    </template>
     <div class="flex w-full items-center justify-end space-x-2 pt-4">
       <Button variant="outline" :text="cancelText" :disabled="loading" @click="close" />
       <Button :variant="variant" :text="confirmText" :loading="loading" @click="emit('confirm')" />
