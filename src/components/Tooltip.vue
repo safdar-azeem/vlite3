e6bcaf5ddbbeef0163a26ae0b5d3dd02d1f0f0f8
safdar-azeem <safdarazeem00@gmail.com>
2026-02-11 19:15:57 +0500

    Update Tooltip.vue

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -3,18 +3,22 @@ import ToolTipLite from 'v-tooltip-lite'
 import 'v-tooltip-lite/style.css'
 import type { TooltTipProps } from 'v-tooltip-lite/types'
 
-const props = withDefaults(defineProps<TooltTipProps>(), {
+const props = withDefaults(defineProps<TooltTipProps & { disabled?: boolean }>(), {
   placement: 'top',
   class: '',
   teleport: true,
   arrow: true,
   contentClass: 'max-w-[350px]',
   triggerClass: '',
+  disabled: false,
 })
 </script>
 
 <template>
-  <ToolTipLite v-bind="props" class="inline-block" class-name="basic-tooltip">
+  <div v-if="disabled" class="contents">
+    <slot />
+  </div>
+  <ToolTipLite v-else v-bind="props" class="inline-block" class-name="basic-tooltip">
     <template #trigger>
       <slot />
     </template>
