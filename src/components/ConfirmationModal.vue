d32e8882034a22d9b0a4f3583d5c1bfb9cb4aa8a
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 15:59:55 +0500

    Update ConfirmationModal.vue

--- src/components/ConfirmationModal.vue
+++ src/components/ConfirmationModal.vue
@@ -25,7 +25,7 @@ const emit = defineEmits<{
   (e: 'confirm'): void
 }>()
 
-const close = () => {
+const cancel = () => {
   emit('cancel')
 }
 </script>
@@ -35,17 +35,33 @@ const close = () => {
     :show="show"
     :title="title"
     :description="description"
+    footerClass="border-t-0! pt-0! pb-3.5!"
     max-width="max-w-[400px]"
-    @close="close"
-    @update:show="(val) => !val && close()">
+    @close="cancel"
+    @update:show="(val) => !val && cancel()">
     <template #trigger>
       <slot name="trigger">
         <slot />
       </slot>
     </template>
-    <div class="flex w-full items-center justify-end space-x-2 pt-4">
-      <Button variant="outline" :text="cancelText" :disabled="loading" @click="close" />
-      <Button :variant="variant" :text="confirmText" :loading="loading" @click="emit('confirm')" />
-    </div>
+    <template #footer="{ close }">
+      <div class="flex w-full items-center justify-end space-x-2">
+        <Button
+          variant="outline"
+          :text="cancelText"
+          :disabled="loading"
+          @click="
+            () => {
+              close?.()
+              cancel?.()
+            }
+          " />
+        <Button
+          :variant="variant"
+          :text="confirmText"
+          :loading="loading"
+          @click="emit('confirm')" />
+      </div>
+    </template>
   </Modal>
 </template>
