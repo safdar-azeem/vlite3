a5442e8324f9345c13d26fab7f10878ff782cdba
safdar-azeem <safdarazeem00@gmail.com>
2026-02-14 12:37:53 +0500

    Update CheckBoxDemo.vue

--- src/playground/demos/CheckBoxDemo.vue
+++ src/playground/demos/CheckBoxDemo.vue
@@ -2,6 +2,8 @@
 import CheckBox from '@/components/CheckBox.vue'
 import Label from '@/components/Label.vue'
 import { ref } from 'vue'
+import DemoSection from '../DemoSection.vue'
+import sourceCode from './CheckBoxDemo.vue?raw'
 
 const checked1 = ref(false)
 const checked2 = ref(true)
@@ -15,8 +17,7 @@ const checked3 = ref(false)
       <p class="text-gray-500">A control that allows the user to make a binary choice.</p>
     </div>
 
-    <section class="space-y-4">
-      <h3 class="text-lg font-semibold">Basic</h3>
+    <DemoSection title="Basic" :code="sourceCode">
       <div class="flex flex-col gap-4">
         <div class="flex items-center gap-2">
           <CheckBox v-model:checked="checked1" id="c1" />
@@ -24,10 +25,9 @@ const checked3 = ref(false)
         </div>
         <p class="text-xs text-gray-400">Value: {{ checked1 }}</p>
       </div>
-    </section>
+    </DemoSection>
 
-    <section class="space-y-4">
-      <h3 class="text-lg font-semibold">States</h3>
+    <DemoSection title="States" :code="sourceCode">
       <div class="flex flex-col gap-4">
         <div class="flex items-center gap-2">
           <CheckBox checked disabled id="c2" />
@@ -38,6 +38,6 @@ const checked3 = ref(false)
           <Label for="c3" class="opacity-50">Unchecked & Disabled</Label>
         </div>
       </div>
-    </section>
+    </DemoSection>
   </div>
 </template>
