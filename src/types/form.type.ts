4e603a8adbc5054c30b7ee3909409b3e35b4a35d
safdar-azeem <safdarazeem00@gmail.com>
2026-02-16 03:59:54 +0500

    Update form.type.ts

--- src/types/form.type.ts
+++ src/types/form.type.ts
@@ -1,75 +1,59 @@
 export type InputType =
-	| 'text'
-	| 'password'
-	| 'email'
-	| 'number'
-	| 'date'
-	| 'tel'
-	| 'url'
-	| 'search'
-	| 'textarea'
+  | 'text'
+  | 'password'
+  | 'email'
+  | 'number'
+  | 'date'
+  | 'tel'
+  | 'url'
+  | 'search'
+  | 'textarea'
 
 export type InputVariant = 'solid' | 'outline' | 'outline-b' | 'transparent'
 export type InputSize = 'sm' | 'md' | 'lg'
-export type InputRounded =
-	| 'none'
-	| 'sm'
-	| 'md'
-	| 'lg'
-	| 'xl'
-	| '2xl'
-	| 'full'
-	| string
+export type InputRounded = 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full' | string
 export type LabelPosition = 'top' | 'left' | 'right'
 
 export interface InputProps {
-	modelValue?: string | number
-	type?: InputType
-	label?: string
-	placeholder?: string
-	disabled?: boolean
-	loading?: boolean
-	error?: string
-	icon?: string
-	iconRight?: string
-	showClearButton?: boolean
-	autofocus?: boolean
-	class?: string
-	inputClass?: string
-	rows?: number
-	lazy?: boolean
-	variant?: InputVariant
-	size?: InputSize
-	rounded?: InputRounded
-	labelPosition?: LabelPosition
-	addonLeft?: string
-	addonRight?: string
-	addonLeftClass?: string
-	addonRightClass?: string
-	min?: number
-	max?: number
+  modelValue?: string | number
+  type?: InputType
+  label?: string
+  placeholder?: string
+  disabled?: boolean
+  loading?: boolean
+  error?: string
+  icon?: string
+  iconRight?: string
+  showClearButton?: boolean
+  autofocus?: boolean
+  class?: string
+  inputClass?: string
+  rows?: number
+  lazy?: boolean
+  variant?: InputVariant
+  size?: InputSize
+  rounded?: InputRounded
+  labelPosition?: LabelPosition
+  addonLeft?: string | Record<string, any>
+  addonRight?: string | Record<string, any>
+  addonLeftClass?: string
+  addonRightClass?: string
+  min?: number
+  max?: number
 }
 
 // Checkbox Types
 export type CheckboxSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-export type CheckboxRounded =
-	| 'none'
-	| 'xs'
-	| 'sm'
-	| 'md'
-	| 'lg'
-	| 'xl'
-	| '2xl'
-	| 'full'
+export type CheckboxRounded = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full'
 
 export interface CheckboxProps {
-	modelValue?: boolean
-	checked?: boolean // Alias
-	disabled?: boolean
-	indeterminate?: boolean
-	label?: string
-	id?: string
-	class?: string
-	size?: CheckboxSize
-	rounded?: CheckboxRounded
+  modelValue?: boolean
+  checked?: boolean // Alias
+  disabled?: boolean
+  indeterminate?: boolean
+  label?: string
+  id?: string
+  class?: string
+  size?: CheckboxSize
+  rounded?: CheckboxRounded
 }
