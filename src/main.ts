75184b6aa1974ed69ff4c2322a0fa09535da97be
safdar-azeem <safdarazeem00@gmail.com>
2026-02-16 20:53:22 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import './css/main.css'
 import App from './App.vue'
 import router from './router'
 import { vScrollReveal } from './directives/vScrollReveal'
+import { env } from './utils'
 
 const app = createApp(App)
 
 // Register global directives
 app.directive('scroll-reveal', vScrollReveal)
+
+// Register Google Sign-In Plugin for script injection
+// app.use(GoogleSignInPlugin, {
+//   clientId: env.VITE_GOOGLE_CLIENT_ID,
+// })
 
 app.use(router)
 app.mount('#app')
