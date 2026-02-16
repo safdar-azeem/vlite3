77a12e89395a5251f914474f2c0d08eed0dbc8b5
safdar-azeem <safdarazeem00@gmail.com>
2026-02-16 16:54:35 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import './css/main.css'
 import App from './App.vue'
 import router from './router'
+import { vScrollReveal } from './directives/vScrollReveal'
 
 const app = createApp(App)
+
+// Register global directives
+app.directive('scroll-reveal', vScrollReveal)
+
 app.use(router)
 app.mount('#app')
