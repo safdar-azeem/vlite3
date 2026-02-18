b77e4bb6b5dae1959557fd608dba8f008fb5695d
safdar-azeem <safdarazeem00@gmail.com>
2026-02-18 19:21:35 +0500

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -50,7 +50,7 @@ const classes = computed(() => {
     xs: 'h-6.5 px-2',
     sm: 'h-8 px-3',
     md: 'h-9 px-4 py-2',
-    lg: 'h-10 px-8',
+    lg: 'h-10 px-6',
     xl: 'h-12 px-10',
   }
   const iconSizes: Record<ButtonSize, string> = {
