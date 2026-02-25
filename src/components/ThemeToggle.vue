948e7d256c5a8f05ebc3092a9927640fe8f3e32c
safdar-azeem <safdarazeem00@gmail.com>
2026-02-26 03:51:55 +0500

    Update ThemeToggle.vue

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import { ButtonRounded, ButtonSize, ButtonVariant } from '@/types'
 import { useTheme } from '../composables/useTheme'
 import Button from './Button.vue'
+
+const props = defineProps<{
+  class?: string
+  rounded?: ButtonRounded
+  size?: ButtonSize
+  variant?: ButtonVariant
+}>()
 
 const { theme, toggleTheme } = useTheme()
 </script>
@@ -9,7 +17,10 @@ const { theme, toggleTheme } = useTheme()
   <slot :toggleTheme="toggleTheme" :theme="theme">
     <Button
       :icon="theme === 'light' ? 'lucide:sun' : 'lucide:moon'"
-      variant="secondary"
+      :variant="variant || 'secondary'"
+      :rounded="rounded || 'md'"
+      :size="size"
+      :class="class"
       :title="theme === 'light' ? 'Switch to dark mode' : 'Switch to light mode'"
       @click="toggleTheme" />
   </slot>
