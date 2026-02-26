7ddd375544ac59928eb0a5f5cd62d44a5f0e7340
safdar-azeem <safdarazeem00@gmail.com>
2026-02-27 03:25:52 +0500

    Update index.ts

--- src/components/Pagination/index.ts
+++ src/components/Pagination/index.ts
@@ -1,1 +1,22 @@
 export { default as Pagination } from './Pagination.vue'
+
+export interface PageInfo {
+  currentPage: number
+  totalPages: number
+  totalItems?: number
+}
+
+export interface PaginationProps {
+  currentPage?: number
+  totalPages: number
+  totalItems?: number
+  disabled?: boolean
+  showEdges?: boolean
+  // New Feature Props
+  showPageInfo?: boolean // "Page 1 of 10"
+  showItemsPerPage?: boolean // "Show 10"
+  itemsPerPage?: number
+  itemsPerPageOptions?: number[]
+  navType?: 'text' | 'icon' // Default 'text' (responsive) vs 'icon' (always arrows)
+  alignment?: 'start' | 'center' | 'end' | 'between'
+}
