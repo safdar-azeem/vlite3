8331265d8d90c2f11a022a0b8c7e82980a18a637
safdar-azeem <safdarazeem00@gmail.com>
2026-02-28 17:10:00 +0500

    Update WorkbookAddButton.vue

--- src/components/Workbook/WorkbookAddButton.vue
+++ src/components/Workbook/WorkbookAddButton.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import Button from '@/components/Button.vue'
+import { $t } from '@/utils/i18n'
 
 const emit = defineEmits<{
   (e: 'click', event: MouseEvent): void
 }>()
+
+const titleText = computed(() => {
+  const res = $t('vlite.workbook.addSheet')
+  return res !== 'vlite.workbook.addSheet' ? res : 'Add New Sheet'
+})
 </script>
 
 <template>
@@ -11,7 +18,8 @@ const emit = defineEmits<{
     variant="ghost"
     size="sm"
     icon="lucide:plus"
-    title="Add New Sheet"
+    :title="titleText"
     rounded="none"
     @click="emit('click', $event)" />
 </template>
+
