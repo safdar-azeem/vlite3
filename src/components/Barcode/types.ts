08b411b6a55886a29fa25f46cc318b358e0ccaf9
safdar-azeem <safdarazeem00@gmail.com>
2026-03-02 21:04:50 +0500

    Add types.ts

---
+++ src/components/Barcode/types.ts
@@ -0,0 +1,12 @@
+export const barcodesConstants = [
+  { value: 'CODE128', label: 'CODE128  (30 chars)', length: 30 },
+  { value: 'CODE39', label: 'code39  (30 chars)', length: 30 },
+  { value: 'UPC', label: 'upc  (14 chars) ', length: 14 },
+  { value: 'EAN13', label: 'ean13  (13 chars)', length: 13 },
+  { value: 'ITF14', label: 'itf14  (13 chars)', length: 13 },
+  { value: 'EAN8', label: 'ean8  (8 chars)', length: 8 },
+  { value: 'EAN5', label: 'ean5  (5 chars)', length: 5 },
+  { value: 'MSI', label: 'msi  (6 chars)', length: 6 },
+  { value: 'pharmacode', label: 'pharmacode  (6 chars)', length: 6 },
+  { value: 'EAN2', label: 'ean2  (2 chars)', length: 2 },
+]
