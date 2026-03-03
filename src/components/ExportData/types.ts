3ee9e03c2f8e1f70f6868599a56ade6fbe532c6e
safdar-azeem <safdarazeem00@gmail.com>
2026-03-03 15:03:29 +0500

    Add types.ts

---
+++ src/components/ExportData/types.ts
@@ -0,0 +1,17 @@
+export interface ExportField {
+  field: string
+  title: string
+  format?: (value: any, row: any) => any
+}
+
+export type ExportFormat = 'excel' | 'csv' | 'json'
+
+export interface ExportDataProps {
+  data: any[]
+  title?: string
+  fields: ExportField[]
+  formats?: ExportFormat[]
+  filename?: string
+  buttonText?: string
+  buttonIcon?: string
+}
