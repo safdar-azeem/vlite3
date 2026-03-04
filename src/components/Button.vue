d1d4c76b1dd118dbb95cecd9a33d752f118e35cc
safdar-azeem <safdarazeem00@gmail.com>
2026-03-04 20:20:29 +0500

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -45,6 +45,8 @@ const classes = computed(() => {
 
     outline:
       'border border-input bg-background hover:bg-accent hover:text-accent-foreground text-foreground',
+    'outline-floating':
+      'border border-input bg-transparent hover:bg-transparent text-foreground border-input',
     'outline-primary': 'border border-primary text-primary hover:bg-primary/10',
     'outline-danger': 'border border-destructive text-destructive hover:bg-destructive/10',
     'outline-warning': 'border border-warning text-warning hover:bg-warning/10',
