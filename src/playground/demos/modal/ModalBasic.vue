e36473543490b8f7c0ee34fb975f79a27350b127
safdar-azeem <safdarazeem00@gmail.com>
2026-03-08 19:12:04 +0500

    Update ModalBasic.vue

--- src/playground/demos/modal/ModalBasic.vue
+++ src/playground/demos/modal/ModalBasic.vue
@@ -26,13 +26,13 @@ const showModal = ref(false)
               <label class="block text-sm font-medium text-gray-700">Username</label>
               <input
                 type="text"
-                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                class="mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
                 value="safdar_azeem" />
             </div>
             <div>
               <label class="block text-sm font-medium text-gray-700">Role</label>
               <select
-                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
+                class="mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
                 <option>Administrator</option>
                 <option>Editor</option>
                 <option>Viewer</option>
