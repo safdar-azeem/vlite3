2703bdca03e4d1907d0c7e985c48704e679ee44a
safdar-azeem <safdarazeem00@gmail.com>
2026-03-13 06:08:08 +0500

    Update UserTable.vue

--- src/playground/demos/ScreenDemoComponents/UserTable.vue
+++ src/playground/demos/ScreenDemoComponents/UserTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { DataTable } from '@/components/DataTable'
+import { DataTable, TableHeader } from '@/components/DataTable'
 import Badge from '@/components/Badge.vue'
 import Avatar from '@/components/Avatar.vue'
 
@@ -8,8 +8,8 @@ defineProps<{
   loading: boolean
 }>()
 
-const headers = [
-  { field: 'name', title: 'User' },
+const headers: TableHeader[] = [
+  { field: 'name', title: 'User', sortable: true },
   { field: 'email', title: 'Email' },
   { field: 'role', title: 'Role', capitalize: true },
   { field: 'status', title: 'Status', capitalize: true },
@@ -17,7 +17,7 @@ const headers = [
 </script>
 
 <template>
-  <DataTable :rows="data" :headers="headers" :loading="loading" :show-pagination="false">
+  <DataTable :rows="data" :headers="headers" :loading="loading" :show-pagination="false" sortable>
     <template #name="{ value, row }">
       <div class="flex items-center gap-3">
         <Avatar :src="row.avatar" :alt="value" />
