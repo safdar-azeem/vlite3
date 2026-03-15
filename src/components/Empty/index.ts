4227579d35af8a8ccff8c1cf50cbaf2f985a0f8b
safdar-azeem <safdarazeem00@gmail.com>
2026-03-16 01:10:32 +0500

    Update index.ts

--- src/components/Empty/index.ts
+++ src/components/Empty/index.ts
@@ -1,1 +1,14 @@
 export { default as Empty } from './Empty.vue'
+
+export type EmptyVariant =
+  | 'variant1'
+  | 'variant2'
+  | 'variant3'
+  | 'variant4'
+  | 'variant5'
+  | 'variant6'
+  | 'variant7'
+  | 'variant8'
+  | 'variant9'
+  | 'variant10'
+  | 'variant11'
