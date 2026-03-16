1e5598a5b0c63d9a65defab8d87ef93e446f5059
safdar-azeem <safdarazeem00@gmail.com>
2026-03-16 18:30:49 +0500

    Update Variant1.vue

--- src/components/Empty/variants/Variant1.vue
+++ src/components/Empty/variants/Variant1.vue
@@ -4,11 +4,12 @@ defineProps<{ title: string; description: string; icon: string }>()
 </script>
 
 <template>
-  <div class="flex w-full flex-col items-center justify-center p-10 md:p-16 text-center">
-    <Icon :icon="icon" class="mb-3 h-10 w-10 text-muted" stroke-width="1.2" />
-    <h3 class="text-2xl font-medium text-foreground">{{ title }}</h3>
+  <div
+    class="flex w-full flex-col items-center justify-center p-10 min-h-[450px] text-center rounded-2xl bg-card">
+    <Icon :icon="icon" class="mb-3 h-12 w-12 text-muted" stroke-width="1.2" />
+    <h3 class="text-fs-5 font-semibold text-foreground">{{ title }}</h3>
     <p class="mt-1.5 text-sm text-muted max-w-md mx-auto" v-html="description"></p>
-    <div class="mt-5" v-if="$slots.action">
+    <div class="mt-6" v-if="$slots.action">
       <slot name="action" />
     </div>
   </div>
