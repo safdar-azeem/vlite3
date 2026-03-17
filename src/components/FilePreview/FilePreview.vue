6bd42df21000b503821c02f85e3ea79b82392422
safdar-azeem <safdarazeem00@gmail.com>
2026-03-18 03:09:13 +0500

    Add FilePreview.vue

---
+++ src/components/FilePreview/FilePreview.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { VFileViewer } from 'v-file-preview'
+import 'v-file-preview/dist/style.css'
+import type { FilePreviewProps } from './types'
+
+const props = defineProps<FilePreviewProps>()
+</script>
+
+<template>
+  <div class="flex flex-col h-[70vh] w-full min-h-[400px]">
+    <div
+      class="flex-1 relative bg-muted/10 rounded-lg border border-border overflow-hidden flex items-center justify-center">
+      <div class="w-full h-full overflow-auto">
+        <VFileViewer :url="url" :name="name" />
+      </div>
+    </div>
+  </div>
+</template>
