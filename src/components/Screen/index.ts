a01c38f94dc9cfbd6c7ca73b0aeecaad9793731c
safdar-azeem <safdarazeem00@gmail.com>
2026-03-18 07:04:01 +0500

    Update index.ts

--- src/components/Screen/index.ts
+++ src/components/Screen/index.ts
@@ -1,3 +1,4 @@
 export { default as Screen } from './Screen.vue'
 export { default as ScreenFilter } from './ScreenFilter.vue'
+export { default as ScreenQuickFilters } from './components/ScreenQuickFilters.vue'
 export * from './types'
