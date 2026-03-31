a7f40508d54e99b190171dac084a57d5b641b7b8
safdar-azeem <safdarazeem00@gmail.com>
2026-04-01 03:57:43 +0500

    Update Invoice.vue

--- src/components/Invoice/Invoice.vue
+++ src/components/Invoice/Invoice.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { getComponentConfig } from '@/utils/configUtils'
 import type { InvoiceProps } from './types'
 import InvoiceVariant1 from './InvoiceVariant1.vue'
 import InvoiceVariant2 from './InvoiceVariant2.vue'
 import InvoiceVariant3 from './InvoiceVariant3.vue'
 import InvoiceVariant4 from './InvoiceVariant4.vue'
 
-const props = withDefaults(defineProps<InvoiceProps>(), {
-  variant: 'Variant1',
-  compact: false,
-  displayBarcodeValue: false,
-})
+const props = defineProps<InvoiceProps>()
+
+const globalConfig = getComponentConfig('invoice') || {}
+
+const variant = computed(() => props.variant ?? globalConfig.variant ?? 'Variant1')
+const compact = computed(() => props.compact ?? globalConfig.compact ?? false)
+const displayBarcodeValue = computed(() => props.displayBarcodeValue ?? globalConfig.displayBarcodeValue ?? false)
 
 const resolvedVariant = computed(() => {
-  switch (props.variant) {
+  switch (variant.value) {
     case 'Variant2':
       return InvoiceVariant2
     case 'Variant3':
@@ -28,5 +31,11 @@ const resolvedVariant = computed(() => {
 </script>
 
 <template>
-  <component :is="resolvedVariant" v-bind="props" />
+  <component 
+    :is="resolvedVariant" 
+    :data="props.data"
+    :variant="variant"
+    :compact="compact"
+    :display-barcode-value="displayBarcodeValue"
+  />
 </template>
