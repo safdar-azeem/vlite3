fccaad0806a23d349c1e60fbe592dcb59c57825c
safdar-azeem <safdarazeem00@gmail.com>
2026-04-05 03:37:09 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ export * from './components/CustomFieldsDisplay'
 export * from './components/Invoice'
 export * from './components/CategoryManager'
 export * from './components/ThumbnailSelector'
+export * from './components/TagInput'
 export * from './components/Calendar'
 
 export { default as Icon } from './components/Icon.vue'
