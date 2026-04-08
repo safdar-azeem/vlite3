27c38e2ddd8ba11aa5415415aeb505bf49b9f404
safdar-azeem <safdarazeem00@gmail.com>
2026-04-09 04:35:44 +0500

    Add index.ts

---
+++ src/components/Skeleton/index.ts
@@ -0,0 +1,4 @@
+export { default as Skeleton } from './Skeleton.vue'
+export * from './types'
+export * from './extract'
+export { registerBones, configureSkeleton, getGlobalConfig } from './shared'
