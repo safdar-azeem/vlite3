f9baee6b0533e5949e75c2421cd48824269c2ed3
safdar-azeem <safdarazeem00@gmail.com>
2026-04-14 03:45:06 +0500

    Update vite.config.vercel.ts

--- vite.config.vercel.ts
+++ vite.config.vercel.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), vueJsx(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
