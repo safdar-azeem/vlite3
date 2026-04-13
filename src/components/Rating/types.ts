93b35512339d835afe7cb904a15ea43dba03c6aa
safdar-azeem <safdarazeem00@gmail.com>
2026-04-14 02:57:16 +0500

    Add types.ts

---
+++ src/components/Rating/types.ts
@@ -0,0 +1,29 @@
+export interface RatingProps {
+  modelValue?: number
+  max?: number
+  allowHalf?: boolean
+  readonly?: boolean
+  showText?: boolean
+  icon?: string | string[]
+  activeIcon?: string | string[]
+  activeColor?: string
+  inactiveColor?: string
+  size?: 'sm' | 'md' | 'lg' | 'xl'
+  clearable?: boolean
+  disabled?: boolean
+  class?: string
+}
+
+export interface ReviewSummaryData {
+  rating: number
+  percentage: number
+  count?: number
+}
+
+export interface ReviewSummaryProps {
+  ratingDistribution: ReviewSummaryData[]
+  totalReviews?: number
+  averageRating?: number
+  showTotal?: boolean
+  class?: string
+}
