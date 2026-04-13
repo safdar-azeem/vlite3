6bdc75a51b94982e4abf5dd250934d2fd960e416
safdar-azeem <safdarazeem00@gmail.com>
2026-04-13 16:12:23 +0500

    Update Price.vue

--- src/components/Price/Price.vue
+++ src/components/Price/Price.vue
@@ -6,6 +6,7 @@ const props = withDefaults(
 	defineProps<{
 		value?: number | string
 		currency?: string
+		format?: 'standard' | 'compact'
 	}>(),
 	{
 		value: 0,
@@ -18,13 +19,22 @@ const activeCurrency = computed(() => {
 	return props.currency || config?.components?.price?.currency || 'USD'
 })
 
+const activeFormat = computed(() => {
+	return props.format || config?.components?.price?.format || 'standard'
+})
+
 const formattedPrice = computed(() => {
     const numericValue = Number(props.value) || 0
 	try {
-		return new Intl.NumberFormat('en-US', {
+        const options: Intl.NumberFormatOptions = {
 			style: 'currency',
 			currency: activeCurrency.value,
-		}).format(numericValue)
+		}
+        if (activeFormat.value === 'compact') {
+            options.notation = 'compact'
+            options.compactDisplay = 'short'
+        }
+		return new Intl.NumberFormat('en-US', options).format(numericValue)
 	} catch (e) {
 		return `$${numericValue.toFixed(2)}`
 	}
