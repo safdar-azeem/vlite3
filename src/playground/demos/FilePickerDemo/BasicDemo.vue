ff1c381e95d7b0b910a06c662ec6cc15d6a51578
safdar-azeem <safdarazeem00@gmail.com>
2026-04-18 01:24:23 +0500

    Add BasicDemo.vue

---
+++ src/playground/demos/FilePickerDemo/BasicDemo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { FilePicker, type FilePickerValue } from '@/components/FilePicker'
+import DemoSection from '../../DemoSection.vue'
+// @ts-ignore
+import sourceCode from './BasicDemo.vue?raw'
+
+const file = ref<FilePickerValue | null>(null)
+</script>
+
+<!-- @demo-snippet Basic Dropzone -->
+<template>
+  <DemoSection title="Basic Dropzone" :code="sourceCode">
+    <div class="w-full max-w-lg">
+      <FilePicker v-model="file" allow-rename />
+    </div>
+  </DemoSection>
+</template>
