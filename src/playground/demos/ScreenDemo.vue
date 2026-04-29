b705d2c1c8651586c085ab205a5ca9a5d7374012
safdar-azeem <safdarazeem00@gmail.com>
2026-04-30 04:15:20 +0500

    Update ScreenDemo.vue

--- src/playground/demos/ScreenDemo.vue
+++ src/playground/demos/ScreenDemo.vue
@@ -123,10 +123,6 @@ const filterType = ref<'dropdown' | 'sidebar'>('dropdown')
         :skip-quick-filter-views="['list']"
         :filter-schema="[
           { name: 'user', type: 'text', label: 'User Name' },
-          { name: 'user', type: 'text', label: 'User Name' },
-          { name: 'user', type: 'text', label: 'User Name' },
-          { name: 'user', type: 'text', label: 'User Name' },
-          { name: 'user', type: 'text', label: 'User Name' },
           {
             name: 'userType',
             label: 'User Type',
